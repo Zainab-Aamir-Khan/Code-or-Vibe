0e8d6010dbe685e377f23cc4c9f6887307d15e7c
ZainabAamir <zaamir427@gmail.com>
2026-07-15 23:43:15 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import BlogPage from './pages/BlogPage'
+import PostPage from './pages/PostPage'
+import Layout from './layouts/MainLayout'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="blog" element={<BlogPage />} />
+        <Route path="blog/:slug" element={<PostPage />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
